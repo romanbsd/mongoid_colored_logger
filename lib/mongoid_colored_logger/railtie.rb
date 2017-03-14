dd02b2584bf1a2f6440989f80da5e72d18cf865d
Stefan Adamov <stefan.adamof@gmail.com>
2017-03-14 08:58:09 +0200

    Fixes the version comparison
    The problem was that version 5.0.2 is returning 5.0 when to_f-ed.
    None of the version 5 Mongoids have Moped.

--- lib/mongoid_colored_logger/railtie.rb
+++ lib/mongoid_colored_logger/railtie.rb
@@ -4,7 +4,7 @@ module MongoidColoredLogger
 
   class Railtie < Rails::Railtie
     mongoid_version = Mongoid::VERSION.to_f
-    base = mongoid_version >= 3.0 && mongoid_version <= 5.0 ? Moped : config.mongoid
+    base = mongoid_version >= 3.0 && mongoid_version < 5.0 ? Moped : config.mongoid
 
     initializer 'logger_decorator', :after => :initialize_logger do
       base.logger = MongoidColoredLogger::LoggerDecorator.new(Rails.logger)
